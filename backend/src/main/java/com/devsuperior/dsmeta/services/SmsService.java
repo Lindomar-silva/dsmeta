739e433e1cf6a231137b3075e8d3d2d33d8493a8
Lindomar-silva <lindomar.s.silva@gmail.com>
2022-07-16 22:09:03 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -36,7 +36,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com um total de R$ " +  sale.getAmount();
+				+ " com um total de R$ " + sale.getAmount();
 
 //		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 //		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
